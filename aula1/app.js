c414fb9eab092fb0de58dde763ff81c23a4e3e2d
Luiz Spinelli <luiz.lucchi@gmail.com>
2019-12-09 21:12:32 -0300

    Create app.js

---
+++ aula1/app.js
@@ -0,0 +1,9 @@
+
+// var, let e const
+// global, escopo, única
+
+const modulo1 = require('./mod1.js')
+const modulo2 = require('./mod2.js')
+
+modulo1('Teste export global')
+modulo2.mensagem('Teste export nomeado')
